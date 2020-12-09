e8e3eb848cee214482e59085839eed4389cafbc3
FajFs <felix.andersson-johansson@teliacompany.com>
2020-12-09 13:49:06 +0100

    Refactored day 8

--- 2020/day8.py
+++ 2020/day8.py
@@ -1,15 +1,7 @@
 import copy
 
-class Inst:
-    def __init__(self, op:str, arg:int, offset:int):
-        self.op = op
-        self.arg = arg
-        self.offset = offset
-
-
 def execute(ins) -> (int, str):
-    acc = 0
-    ptr = 0
+    acc, ptr = 0, 0
     executedInstructions = set()
 
     while True:
@@ -35,40 +27,35 @@ def execute(ins) -> (int, str):
             ptr += ins[ptr].arg
             continue
 
-
-
-def part2(instructions:list):
+def part2(instructions:list) -> (int, str):
     i = instructions[:]
     for idx in range(len(i)):
         i = copy.deepcopy(instructions)
-        acc = 0
-        err = ""
+        acc, err = 0, ""
         if i[idx].op == "jmp":
             i[idx].op = "nop"
             acc, err = execute(i)
-
         elif i[idx].op == "nop":
             i[idx].op = "jmp"
             acc, err = execute(i)
         
-       # print(acc, err)
         if err == "ok":
-            print(acc, err)
-            return
+            return acc, err
+
+class Inst:
+    def __init__(self, op:str, arg:int, offset:int):
+        self.op = op
+        self.arg = arg
+        self.offset = offset
 
 def main():
     inp = [x.strip() for x in open("aoc/2020/inputfiles/day8.in", 'r').readlines()]
-    # tree structure 
-    #filter strings.
     instructions = []
     for i, ins in enumerate(inp):
         op, arg = ins.split()
         instructions.append(Inst(op, int(arg), i))
-    print(execute(instructions))
-    part2(instructions)
-
-
-    
+    print("part 1: ", execute(instructions))
+    print("part 2: ", part2(instructions))
 
 if __name__ == "__main__":
     main()
