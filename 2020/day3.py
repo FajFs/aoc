2d6912223a37c6cf16ed6e4f8db6822501c98eb5
FajFs <felix.andersson-johansson@teliacompany.com>
2020-12-09 11:46:59 +0100

    Refactored day 3

--- 2020/day3.py
+++ 2020/day3.py
@@ -1,46 +1,27 @@
-test = ["..##.......", "#...#...#..", ".#....#..#.", "..#.#...#.#", ".#...##..#.", "..#.##.....", ".#.#.#....#", ".#........#", "#.##...#...", "#...##....#", ".#..#...#.#"]
-
-def part1(d):
-    c = 0
-    r = 0
-    ans = 0
-    while r < len(d)-1:
-        r += 1
-        c += 3
-        if d[r][c%len(d[r])] == "#":
+def part1(inp: list) -> int:
+    col, row, ans = 0, 0, 0
+    while row < len(inp) - 1:
+        row, col = row + 1, col + 3
+        if inp[row][col % len(inp[row])] == "#":
             ans += 1
-    print(ans)
-
-    
+    return ans
 
 move = [(1,1), (1,3), (1,5), (1,7), (2,1)]
-
-def part2(d):
+def part2(inp: list) -> int:
     ans = 1
-    for row, col in move:
-        c = 0
-        r = 0
-        val = 0
-        while r < len(d)-1:
-            r += row
-            c += col
-            if d[r][c%len(d[r])] == "#":
+    for down, right in move:
+        col, row, val = 0, 0, 0
+        while row < len(inp)-1:
+            row, col = row + down, col + right
+            if inp[row][col % len(inp[row])] == "#":
                 val += 1
         ans *= val
-    print(ans)
+    return ans
         
-
-
 def main():
-    inp = open("aoc/2020/inputfiles/day3.in", 'r').readlines()
-    graph = []
-    for line in inp:
-        graph.append(line.strip())
-        
-    part1(graph)
-    part2(graph)
-
-
+    inp = [x.strip() for x in open("aoc/2020/inputfiles/day3.in", 'r').readlines()]
+    print("part 1: ", part1(inp))
+    print("part 2: ", part2(inp))
 
 if __name__ == "__main__":
     main()
