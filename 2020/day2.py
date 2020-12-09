4f010fbca2d6f5612ec6924f1c9ca8c90786ace5
FajFs <felix.andersson-johansson@teliacompany.com>
2020-12-09 11:30:44 +0100

    Refactored day 2

--- 2020/day2.py
+++ 2020/day2.py
@@ -1,32 +1,26 @@
 import re
 
-test = ["1-3 a: abcde","1-3 b: cdefg","2-9 c: ccccccccc"]
-
-def part1(data):
+def part1(inp: list) -> int:
     res = 0
-    for line in data:
-        d = re.split('[-: ]+', line)
-        if int(d[0]) <= d[3].count(d[2]) <= int(d[1]):
+    for line in inp:
+        minVal, maxVal, c, psw = re.split('[-: ]+', line)
+        if int(minVal) <= psw.count(c) <= int(maxVal):
             res = res + 1
-    print(res)
+    return res
 
-
-def part2(data):
+def part2(inp: list) -> int:
     res = 0
-    for line in data:
-        d = re.split('[-: ]+', line)
-        n = d[3][int(d[0])-1]
-        m = d[3][int(d[1])-1]
-        if (n == d[2] and not m == d[2]) or (not n == d[2] and m == d[2]): 
+    for line in inp:
+        posA, posB, c, psw = re.split('[-: ]+', line)
+        ca, cb = psw[int(posA)-1], psw[int(posB)-1]
+        if (ca == c and not cb == c) or (not ca == c and cb == c): 
             res = res + 1
-    print(res)
+    return res
 
 def main():
     inp = open("aoc/2020/inputfiles/day2.in", 'r').readlines()
-    part1(inp)
-    part2(inp)
-
-
+    print("part 1: ", part1(inp))
+    print("part 2: ", part2(inp))
 
 if __name__ == "__main__":
     main()
