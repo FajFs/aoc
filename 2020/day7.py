50b948fbff6ef08323f28069e2c6c9f834bcb17c
FajFs <felix.andersson-johansson@teliacompany.com>
2020-12-09 13:42:16 +0100

    Refactored day 7

--- 2020/day7.py
+++ 2020/day7.py
@@ -1,17 +1,11 @@
 import re
 
-class Bag:
-    mult = 1
-    def __init__(self, v, c):
-        self.value = v
-        self.color = c
-
 #Bottom up
-def part1(d):
+def part1(inp: list) -> int:
     parents = set()
     res = set()
     #prepare first case
-    for (k,v) in d.items():
+    for (k,v) in inp.items():
         if "shinygold" in [x.color for x in v]:
             parents.add(k)
             res.add(k)
@@ -22,26 +16,25 @@ def part1(d):
             if p in [x.color for x in v]:
                 parents.add(k)
                 res.add(k)
-    print("part 1: ", len(res))
-
+    return len(res)
 
 #Top down - recursive preorder node count times node value
-def rec_count(children, fullList):
+def rec_count(children: list, fullList: list) -> int:
     res = 1
     for c in children:
         res += c.value * rec_count(fullList[c.color], fullList)
     return res
 
+def part2(inp: list) -> int:
+    return rec_count(inp["shinygold"], inp) - 1
 
-def part2(d):
-    print("part 2: ", rec_count(d["shinygold"], d) - 1)
-
-
+class Bag:
+    def __init__(self, v, c):
+        self.value = v
+        self.color = c
 
 def main():
     inp = [x.strip() for x in open("aoc/2020/inputfiles/day7.in", 'r').readlines()]
-    # tree structure 
-    #filter strings.
     filteredInput = {}
     for line in inp:
         line = line.replace("bags","").replace("contain","").replace("bag","").replace(".","").replace(" ","").replace(",","").replace("noother","")
@@ -50,14 +43,8 @@ def main():
         for i in range(1, len(tmp), 2):
             bags.append(Bag(int(tmp[i]), tmp[i+1]))
         filteredInput[tmp[0]] = bags
-
-
-
-    part1(filteredInput)
-    part2(filteredInput)
-
-
-
+    print("part 1: ", part1(filteredInput))
+    print("part 2: ", part2(filteredInput))
 
 if __name__ == "__main__":
     main()
