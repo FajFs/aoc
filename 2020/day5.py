1a16f77d812f3a771c690ef4b2e313be32b6ec39
FajFs <felix.andersson-johansson@teliacompany.com>
2020-12-09 13:22:48 +0100

    Refactored day 5

--- 2020/day5.py
+++ 2020/day5.py
@@ -1,42 +1,37 @@
 import math
 
-
-def part12(data):
-    ids = []
-    for line in data:
-        rowHigh = 127
-        rowLow = 0
-        for row in range(0, 7):
-            if line[row] == "F":
-                rowHigh = (rowHigh + rowLow) // 2
-            if line[row] == "B":
-                rowLow = math.ceil((rowHigh + rowLow) / 2)
-        
-        colLow = 8
-        colHigh = 0 
-        for col in range(7,10):
-            if line[col] == "R":
-                colHigh = (colHigh + colLow) // 2
-            if line[col] == "L":
-                colLow = math.ceil((colHigh + colLow) / 2)
-        
-        ids.append(rowHigh * 8 + colHigh)
-    print("p1: ", max(ids))
-
+def part12(inp: list) -> (int, int):
+    id = []
     res = []
-    comp = range(0, max(ids))
-    for i in comp:
-        if i not in ids:
-            res.append(i)
-    print("p2: ", max(res))
 
+    floor = lambda a,b: (a + b) // 2
+    ceil  = lambda a,b: math.ceil((a + b) / 2)
+    
+    for line in inp:
+        rowH, rowL = 127, 0
+        colH, colL = 8, 0
+
+        for c in line:
+            if c == "F": 
+                rowH = floor(rowH, rowL)
+            if c == "B":
+                rowL = ceil(rowH, rowL)
+            if c == "L":
+                colH = floor(colH, colL)
+            if c == "R":
+                colL = ceil(colH, colL)      
+        id.append(rowH * 8 + colL)
+        
+    for i in [x for x in range(0, max(id))]:
+        if i not in id:
+            res.append(i)
+    return max(id), max(res)
 
 def main():
-    inp = open("aoc/2020/inputfiles/day5.in", 'r').readlines()
-    part12(inp)
-    
-
-
+    inp = [x.strip() for x in open("aoc/2020/inputfiles/day5.in", 'r').readlines()]
+    a,b = part12(inp)
+    print("part 1: ", a)
+    print("part 2: ", b)
 
 if __name__ == "__main__":
     main()  
